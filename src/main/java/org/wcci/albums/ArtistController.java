35b761befcec3676449a5c4b5129a33b51c580a2
robby <robby.remmenga@gmail.com>
2019-10-29 13:37:08 -0400

    made controller and test for Artist

--- src/main/java/org/wcci/albums/ArtistController.java
+++ src/main/java/org/wcci/albums/ArtistController.java
@@ -1,5 +1,7 @@
 package org.wcci.albums;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,15 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/artists")
-public class artistController {
+public class ArtistController {
 
 	@Autowired
 	private ArtistRepository artistRepo;
-	@Autowired
-	private AlbumRepository albumRepo;
+
 	
 	@GetMapping("")
-	public Iterable<Artist> fetchAllArtists() {
-		return artistRepo.findAll();
+	public List<Artist> fetchAll() {
+		return (List<Artist>) artistRepo.findAll();
 	}
 }
