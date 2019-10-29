36cf797ece308080fa5f21bb11fd190aa3bb6708
robby <robby.remmenga@gmail.com>
2019-10-29 14:15:36 -0400

    test passes and album controller made

--- src/test/java/org/wcci/albums/AlbumControllerTest.java
+++ src/test/java/org/wcci/albums/AlbumControllerTest.java
@@ -15,6 +15,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.Before;
 import org.junit.Ignore;
@@ -27,13 +28,13 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 public class AlbumControllerTest {
-	
+
 	@InjectMocks
 	private AlbumController underTest;
 
 	@Mock
 	private AlbumRepository albumRepo;
-	
+
 	private Album testAlbum;
 	private MockMvc mockMvc;
 
@@ -43,28 +44,40 @@ public class AlbumControllerTest {
 		mockMvc = MockMvcBuilders.standaloneSetup(underTest).build();
 		testAlbum = new Album("testAlbum", new Artist("Jane"));
 	}
-	
+
 	@Test
 	public void fetchAllReturnsListOfAlbums() throws Exception {
 		when(albumRepo.findAll()).thenReturn(Collections.singletonList(testAlbum));
 		List<Album> retrievedAlbums = underTest.fetchAll();
 		assertThat(retrievedAlbums, contains(testAlbum));
 	}
-	
+
 	@Test
 	public void fetchAllIsMappedCorrectlyAndReturnsAJsonList() throws Exception {
 		when(albumRepo.findAll()).thenReturn(Collections.singletonList(testAlbum));
 		mockMvc.perform(get("/api/albums"))
-			   .andDo(print())
-			   .andExpect(status().isOk())
-			   .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-			   .andExpect(jsonPath("$", hasSize(1)))
-               .andExpect(jsonPath("$[0].title", is(equalTo("testAlbum"))));
+		       .andDo(print())
+		       .andExpect(status().isOk())
+			   .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8)).andExpect(jsonPath("$", hasSize(1)))
+			   .andExpect(jsonPath("$[0].title", is(equalTo("testAlbum"))));
 	}
-	
-	
-	
-	
-	
+
+	@Test
+	public void fetchByIdReturnsSingleAlbum() {
+		when(albumRepo.findById(1L)).thenReturn(Optional.of(testAlbum));
+		Album retrievedAlbum = underTest.fetchById(1L);
+		assertThat(retrievedAlbum, is(testAlbum));
+	}
+
+	@Test
+	public void fetchByIdIsMappedCorrectlyAndReturnsAJsonAlbum() throws Exception {
+		when(albumRepo.findById(1L)).thenReturn(Optional.of(testAlbum));
+		mockMvc.perform(get("/api/albums/1"))
+			   .andDo(print())
+		       .andExpect(status().isOk())
+			   .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+			   .andExpect(jsonPath("$.title", is(equalTo("testAlbum"))));
+
+	}
 
 }
