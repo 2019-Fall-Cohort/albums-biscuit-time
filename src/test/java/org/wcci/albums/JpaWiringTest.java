df9696795a98584b628da12b816fd3bdb4a1c93d
Brian Bross <brian.bross@gmail.com>
2019-10-31 14:57:02 -0400

    artist and albums have tags and passing tests

--- src/test/java/org/wcci/albums/JpaWiringTest.java
+++ src/test/java/org/wcci/albums/JpaWiringTest.java
@@ -10,6 +10,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.wcci.albums.models.Album;
 import org.wcci.albums.models.Artist;
@@ -19,6 +20,7 @@ import org.wcci.albums.models.Tag;
 import org.wcci.albums.repositories.AlbumRepository;
 import org.wcci.albums.repositories.ArtistRepository;
 import org.wcci.albums.repositories.SongRepository;
+import org.wcci.albums.repositories.TagRepository;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
@@ -30,6 +32,8 @@ public class JpaWiringTest {
 	private AlbumRepository albumRepo;
 	@Autowired
 	private SongRepository songRepo;
+	@Autowired
+	private TagRepository tagRepo;
 	@Autowired
 	private TestEntityManager entityManager;
 
@@ -122,19 +126,29 @@ public class JpaWiringTest {
 	}
 	
 	@Test
-	public void albumsCanHaveTags() throws Exception {
+	public void albumsAndArtistsCanHaveTags() throws Exception {
 		Artist testArtist = new Artist("Ben");
 		Album testAlbum = new Album("Greatest Hits", testArtist);
+		
 		testArtist = artistRepo.save(testArtist);
 		testAlbum = albumRepo.save(testAlbum);
+		
 		Tag testTag = new Tag("fantastico");
-		testAlbum.addTag(testTag);
+		
+		testTag = tagRepo.save(testTag);
+		testTag.addAlbum(testAlbum);
+		testTag.addArtist(testArtist);
+		testTag = tagRepo.save(testTag);
 
 		entityManager.flush();
 		entityManager.clear();
 
+		Tag retrievedTag = tagRepo.findById(testTag.getId()).get();
 		Album retrievedAlbum = albumRepo.findById(testAlbum.getId()).get();
-		assertThat(retrievedAlbum.getTags(), contains(testTag));
+		Artist retrievedArtist = artistRepo.findById(testArtist.getId()).get();
+		
+		assertThat(retrievedArtist.getTags(), contains(retrievedTag));
+		assertThat(retrievedAlbum.getTags(), contains(retrievedTag));
 	}
 
 }
