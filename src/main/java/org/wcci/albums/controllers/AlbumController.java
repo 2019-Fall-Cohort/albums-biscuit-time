6e49bcbdf45a960d80b2395b81f4fd2326776165
Brian Bross <brian.bross@gmail.com>
2019-11-08 14:48:25 -0500

    added cross origin annotation to album controller

--- src/main/java/org/wcci/albums/controllers/AlbumController.java
+++ src/main/java/org/wcci/albums/controllers/AlbumController.java
@@ -3,6 +3,7 @@ package org.wcci.albums.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,12 +12,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.wcci.albums.models.Album;
-import org.wcci.albums.models.Artist;
 import org.wcci.albums.models.Comment;
 import org.wcci.albums.models.Tag;
 import org.wcci.albums.repositories.TagRepository;
 import org.wcci.albums.services.AlbumService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/albums")
 public class AlbumController {
